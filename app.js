e6d168ee3e1ff908b1b95898b57f7729036e3d7e
yannou64 <yannickpro64@gmail.com>
2025-09-16 15:07:52 +0200

    Rajout de helmet et des CSP

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import connectionBDD from "./config/dbConnecion.js";
 import authentificationRouter from "./routers/authentificationRouter.js";
 import priseDeContactRouter from "./routers/priseDeContactRouter.js";
 import editionRouter from "./routers/editionRouter.js";
+import helmet from "helmet";
 import cors from "cors";
 
 dotenv.config();
@@ -19,14 +20,39 @@ if (process.env.NODE_ENV === "production") {
   app.set("trust proxy", 1);
 }
 
+// Sécurité
 // Sécurité forcer le https en prod
-app.use((req, res, next) => { 
-	if (process.env.NODE_ENV === "production" && req.header('x-forwarded-proto') !== 'https') { 
-		return res.redirect(`https://${req.header('host')}${req.url}`) 
-	} 
-	next() 
-})
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === "production" && req.header("x-forwarded-proto") !== "https") {
+    return res.redirect(`https://${req.header("host")}${req.url}`);
+  }
+  next();
+});
 
+// Ajout des entêtes de sécurité et du contentSecurityPolicy, attention si j'utilise des CDN il faudra les déclarer
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "default-src": ["'self'"],
+      "script-src": ["'self'"],
+      "style-src": ["'self'"],
+      "img-src": ["'self'", "data:"],
+      "font-src": ["'self'", "data:"],
+      "connect-src": ["'self'"], // ajoute le domaine du front si le front appelle cette API depuis un autre origin
+      "frame-ancestors": ["'self'"],
+    },
+  })
+);
+
+// Les différentes origin autorisées, avec filtre si n'est pas défini dans .env
+const originAuthorise = ["http://localhost:5173", process.env.FRONTEND_ORIGIN].filter(Boolean);
+app.use(
+  cors({
+    origin: originAuthorise,
+    credentials: true,
+  })
+);
 // Lancement du serveur
 app.listen(port, () => {
   console.log(`Le serveur est démarré sur http://localhost:${port}`);
@@ -41,16 +67,6 @@ app.get("/health", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
-
-// Sécurité
-// Les différentes origin autorisées, avec filtre si n'est pas défini dans .env
-const originAuthorise = ["http://localhost:5173", process.env.FRONTEND_ORIGIN].filter(Boolean);
-app.use(
-  cors({
-    origin: originAuthorise,
-    credentials: true,
-  })
-);
 
 // routes du projet
 app.use("/api/auth", authentificationRouter);
