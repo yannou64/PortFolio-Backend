371f87d6c6fef4a999b62260ceb3d9ce923a15d7
yannou64 <yannickpro64@gmail.com>
2025-10-02 16:18:46 +0200

    reglage bug mail en prod

--- controllers/priseDeContactControllers.js
+++ controllers/priseDeContactControllers.js
@@ -1,14 +1,12 @@
-import mail from "../utils/mail.js"
+import mail from "../utils/mail.js";
 
-export async function sendMessage(req, res){
-    try {
-        const {emailContact, message} = req.body
-        // Pas de await pour ne pas avoir un lag avant la redirection vers la page de remerciement
-        mail(emailContact, message)
-        res.status(200).json({message: "Mail sent"})
-    } catch (e) {
-        res.status(500).json({message: "network error for send a message"})
-    }
-    res.status(200).json({message: "alors ?"})
+export async function sendMessage(req, res) {
+  try {
+    const { emailContact, message } = req.body;
+    // Pas de await pour ne pas avoir un lag avant la redirection vers la page de remerciement
+    await mail(emailContact, message);
+    return res.status(200).json({ message: "Mail sent" });
+  } catch (e) {
+    return res.status(500).json({ message: "network error for send a message" });
+  }
 }
-
